e4dd9df3cadb1eff553e0c0dfb924ac23ce475a7
nashucool <nashucool01@gmail.com>
2022-10-03 13:44:34 -0400

    Project react 4 dolla deja terminer

---
+++ src/pages/signin.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ScrollToTop from '../components/ScrollToTop'
+import SignIn from '../components/Signin'
+
+/*ScrollToTop pas necessaire */
+const SigninPage = () => {
+  return (
+    <>
+    <ScrollToTop/>
+    <SignIn/>
+
+    </>
+  )
+}
+
+export default SigninPage
